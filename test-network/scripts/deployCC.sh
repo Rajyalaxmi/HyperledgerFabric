c2105f3b351e9e5fcb3a07430362625a8d0c93c8
Rajyalaxmi Vanam <rajilucky222@gmail.com>
2022-01-04 10:37:49 +0530

    Update deployCC.sh

--- test-network/scripts/deployCC.sh
+++ test-network/scripts/deployCC.sh
@@ -292,16 +292,16 @@ chaincodeQuery() {
 packageChaincode
 
 ## Install chaincode on peer0, and peer1 of all 4 orgs
-# infoln "Installing chaincode on peer0.org1..."
-# installChaincode 1 0
-# infoln "Installing chaincode on peer1.org1..."
-# installChaincode 1 1
-# infoln "Install chaincode on peer0.org2..."
-# installChaincode 2 0
-# infoln "Install chaincode on peer1.org2..."
-# installChaincode 2 1
-# infoln "Install chaincode on peer0.org3..."
-# installChaincode 3 0
+infoln "Installing chaincode on peer0.org1..."
+installChaincode 1 0
+infoln "Installing chaincode on peer1.org1..."
+installChaincode 1 1
+infoln "Install chaincode on peer0.org2..."
+installChaincode 2 0
+infoln "Install chaincode on peer1.org2..."
+installChaincode 2 1
+infoln "Install chaincode on peer0.org3..."
+installChaincode 3 0
 infoln "Install chaincode on peer1.org3..."
 installChaincode 3 1
 infoln "Install chaincode on peer0.org4..."
@@ -314,7 +314,6 @@ queryInstalled 1 1
 
 # ## approve the definition for org1
 approveForMyOrg 1 0
-# approveForMyOrg 1 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect org1 to have approved and org2 not to
@@ -322,7 +321,6 @@ checkCommitReadiness 1 1
 
 ## now approve also for org2
 approveForMyOrg 2 0
-# approveForMyOrg 2 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect them both to have approved
@@ -330,7 +328,6 @@ checkCommitReadiness 2 1
 
 ## now approve also for org3
 approveForMyOrg 3 0
-# approveForMyOrg 3 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect them both to have approved
@@ -338,7 +335,6 @@ checkCommitReadiness 3 1
 
 ## now approve also for org4
 approveForMyOrg 4 0
-# approveForMyOrg 4 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect them both to have approved
@@ -349,20 +345,20 @@ commitChaincodeDefinition 1 2 3 4
 
 ## query on both orgs to see that the definition committed successfully
 queryCommitted 1 0
-# queryCommitted 1 1
+queryCommitted 1 1
 queryCommitted 2 0
-# queryCommitted 2 1
+queryCommitted 2 1
 queryCommitted 3 0
-# queryCommitted 3 1
+queryCommitted 3 1
 queryCommitted 4 0
-# queryCommitted 4 1
+queryCommitted 4 1
 
 ## Invoke the chaincode - this does require that the chaincode have the 'initLedger'
 ## method defined
 if [ "$CC_INIT_FCN" = "NA" ]; then
   infoln "Chaincode initialization is not required"
 else
-  chaincodeInvokeInit 1 2
+  chaincodeInvokeInit 1 2 3 4
   chaincodeQuery 1 1
 fi
 
